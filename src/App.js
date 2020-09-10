f3bcfba8223a47a75373363b57b98ffb6d2899d8
Anna-Pronovich <pronovich.ann@gmail.com>
2020-09-10 13:58:05 +0300

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import { compose } from 'redux';
 
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
+
 import UsersContainer from "./components/Users/UsersContainer";
 import LoginPage from "./components/Login/Login";
+
 import Preloader from "./components/common/Preloader/Preloader";
 
+import { withSuspense } from "./hoc/withSuspense";
 import { initializeApp } from "./redux/redux-thunk/app-thunk";
 
 import './App.css';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+
 class App extends React.Component {
 
     componentDidMount() {
@@ -31,17 +36,18 @@ class App extends React.Component {
                 <HeaderContainer />
                 <Navbar />
                 <div className='app-wrapper-content'>
+
                     <Route path='/dialogs'
-                        render={() => <DialogsContainer />} />
+                        render={withSuspense(DialogsContainer)} />
 
                     <Route path='/profile/:userId?'
-                        render={() => <ProfileContainer />} />
+                        render={withSuspense(ProfileContainer)} />
 
                     <Route path='/users'
                         render={() => <UsersContainer />} />
+
                     <Route path='/login'
                         render={() => <LoginPage />} />
-
                 </div>
             </div>
         )
